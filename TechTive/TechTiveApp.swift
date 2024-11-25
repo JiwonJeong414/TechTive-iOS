f40a71da276bb3af8cc7ffaae1d8c9f4078a4b18
jiwonjeong414 <jiwonjeong414@gmail.com>
2024-11-24 23:43:12 -0500

    initial commit

---
+++ TechTive/TechTiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechTiveApp.swift
+//  TechTive
+//
+//  Created by jiwon jeong on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TechTiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
